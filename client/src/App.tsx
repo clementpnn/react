7240b9a1580aa60eaa07298bac21b2004eaade6b
Clément <clementpnc@gmail.com>
2022-12-07 10:58:49 +0100

    feat: add client

---
+++ client/src/App.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+
+export interface formDataInterface {
+  username: string,
+  email: string,
+  password: string,
+  passwordConfirm: string
+}
+
+function App() {
+
+  const [formData, setFormData] = useState<formDataInterface>({username: '', email: '', password: '', passwordConfirm: ''})
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    fetch('http://localhost:1234', {
+        method: "POST",
+        mode: "cors",
+        body: new URLSearchParams({
+            ...formData
+        }),
+        credentials: "include",
+        headers: new Headers({
+            // "Authorization" : "Basic amZnbWFpbC5jb206cGFzc3dvcmQ=",
+            "Content-type":  "application/x-www-form-urlencoded"
+        })
+    })
+        .then(data => data.text())
+        .then(json => console.log(json))
+  }
+
+  const handleChange = (e: ChangeEvent) => {
+    setFormData(prevState => {
+        return {
+            ...prevState,
+            [e.target.name]: e.target.value
+        }
+    })
+  }
+
+  return (
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='name'>name</label>
+        <input type='text' name='name' onChange={handleChange} />
+
+        <label htmlFor='email'>email</label>
+        <input type='email' name='email' onChange={handleChange} />
+
+        <label htmlFor='password'>password</label>
+        <input type='password' name='password' onChange={handleChange} />
+
+        <label htmlFor='passwordConfirm'>password confirm</label>
+        <input type='password' name='passwordConfirm' onChange={handleChange} />
+
+        <input type="submit" value="submit" />
+      </form>
+  )
+}
+
+export default App
---
+++ client/src/main.tsx
@@ -0,0 +1,41 @@
+import ReactDOM from 'react-dom/client'
+import {Outlet, RouterProvider, createReactRouter, createRouteConfig, Link,} from '@tanstack/react-router'
+import App from './App'
+import Test from './test'
+
+const rootRoute = createRouteConfig()
+
+const indexRoute = rootRoute.createRoute({
+  path: '/',
+  component: () => {
+    return (
+      <App />
+    )
+  },
+})
+
+const testRoute = rootRoute.createRoute({
+  path: '/test',
+  component: () => {
+    return (
+      <Test />
+    )
+  },
+})
+
+const routeConfig = rootRoute.addChildren([
+  indexRoute,
+  testRoute
+])
+
+const router = createReactRouter({
+  routeConfig,
+})
+
+const rootElement = document.getElementById('root') as HTMLElement
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <RouterProvider router={router} />
+  )
+}
